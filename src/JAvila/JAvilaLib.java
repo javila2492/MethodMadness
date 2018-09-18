7fb301d02499c8e9700605054fb5471a427aa3da
javila2492 <javila2492@bths.edu>
2018-09-18 09:40:44 -0400

    v2

--- src/JAvila/JAvilaLib.java
+++ src/JAvila/JAvilaLib.java
@@ -6,4 +6,20 @@ public class JAvilaLib {
     {
         System.out.println(str);
     }
+
+    public static String dateStr(String str)
+    {
+        String month = str.split("/")[0];
+        String day = str.split("/")[1];
+        String year = str.split("/")[2];
+        return day + "-" + month + "-" + year;
+    }
+
+    public static String cutOut(String str1, String str2)
+    {
+        if(str1.indexOf(str2) == -1)
+            return str1;
+        int n = str1.indexOf(str2);
+        return str1.substring(0, n) + str1.substring(n + str2.length());
+    }
 }
--- src/JAvila/TestFile.java
+++ src/JAvila/TestFile.java
@@ -1,5 +1,7 @@
 package JAvila;
 
+import static JAvila.JAvilaLib.cutOut;
+import static JAvila.JAvilaLib.dateStr;
 import static JAvila.JAvilaLib.println;
 
 public class TestFile {
@@ -7,5 +9,8 @@ public class TestFile {
     public static void main(String[] args)
     {
         println("I love");
+        System.out.println(dateStr("6/21/2001"));
+        System.out.println(cutOut("palindrome", "pal"));
     }
+
 }
